3f4264931eccfb2a703268e8c2ee09551d03ba03
Szabó Bence <szabo.bence.tat@gmail.com>
2020-04-09 07:18:14 +0200

    routing, layout, localization

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
 import React from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
+import { IntlProvider } from 'react-intl';
 
-import logo from './logo.svg';
 import './App.css';
+import Layout from "./client/layouts/layout";
+
+import messages_cs from '../localization/cs.json';
+import messages_de from '../localization/de.json';
+import messages_en from '../localization/en.json';
+import messages_es from '../localization/es.json';
+import messages_fi from '../localization/fi.json';
+import messages_fr from '../localization/fr.json';
+import messages_ja from '../localization/ja.json';
+import messages_pt from '../localization/pt.json';
+import messages_tr from '../localization/tr.json';
+import messages_zh from '../localization/zh.json';
 
 function App() {
+  const language = this.props.params.language || 'en';
+
+  const messages = {
+    'cs': messages_cs,
+    'de': messages_de,
+    'en': messages_en,
+    'es': messages_es,
+    'fi': messages_fi,
+    'fr': messages_fr,
+    'ja': messages_ja,
+    'pt': messages_pt,
+    'tr': messages_tr,
+    'zh': messages_zh
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <IntlProvider locale={language} messages={messages[language]}>
+        <div className="App">
+          <Layout>
+            <Switch>
+              <Route path="/:language/about" component={''} />
+              <Route path="/:language/applications" component={''}/>
+              <Route path="/:language/contact" component={''} />
+              <Route path="/:language/newsroom" component={''} />
+              <Route path="/:language/events" component={''} />
+              <Route path="/:language/products" component={''} />
+              <Route path="/:language/why-robotmaster" component={''} />
+              <Route path="/:language/success-stories" component={''} />
+              <Route path="/:language/privacy" component={''} />
+              <Route path="/:language/disclaimer" component={''}/>
+              <Route path="/:language/eula" component={''} />
+              <Route path="/:language/gdpr" component={''} />
+              <Route path="/:language/partners" component={''} />
+              <Route path="/:language" exact component={'Home'}/>
+              <Redirect to="/en"/>
+            </Switch>
+          </Layout>
+        </div>
+      </IntlProvider>
   );
 }
 
--- src/client/layouts/layout.js
+++ src/client/layouts/layout.js
@@ -1,10 +1,18 @@
 import React, {Component} from 'react';
+import Topbar from "../ui/topbar";
+import Navbar from "../ui/navbar";
+import Footer from "../ui/footer";
+import SocialPages from "../ui/social-pages";
 
 export default class Layout extends Component {
   render() {
     return (
         <div className={'layout'}>
+          <Topbar/>
+          <Navbar path={this.props.path}/>
           {this.props.children}
+          <Footer/>
+          <SocialPages/>
         </div>
     );
   }
