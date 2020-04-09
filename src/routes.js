e5673afdfeea4aff10ff875d8a6ffa67330eecb6
Szabó Bence <szabo.bence.tat@gmail.com>
2020-04-09 10:53:25 +0200

    added routes

--- src/routes.js
+++ src/routes.js
@@ -11,6 +11,102 @@ const Routes = [
     loadData: () => loadData('posts')
   },
   {
+    path: '/v6-trial',
+    exact: true,
+    component: Test,
+    loadData: () => loadData('posts')
+  },
+  {
+    path: '/v7-trial',
+    exact: true,
+    component: Test,
+    loadData: () => loadData('posts')
+  },
+  {
+    path: '/about',
+    exact: true,
+    component: Test,
+    loadData: () => loadData('posts')
+  },
+  {
+    path: '/events',
+    exact: true,
+    component: Test,
+    loadData: () => loadData('posts')
+  },
+  {
+    path: '/applications',
+    exact: true,
+    component: Test,
+    loadData: () => loadData('posts')
+  },
+  {
+    path: '/newsroom',
+    exact: true,
+    component: Test,
+    loadData: () => loadData('posts')
+  },
+  {
+    path: '/contact',
+    exact: true,
+    component: Test,
+    loadData: () => loadData('posts')
+  },
+  {
+    path: '/disclaimer',
+    exact: true,
+    component: Test,
+    loadData: () => loadData('posts')
+  },
+  {
+    path: '/eula',
+    exact: true,
+    component: Test,
+    loadData: () => loadData('posts')
+  },
+  {
+    path: '/gdpr',
+    exact: true,
+    component: Test,
+    loadData: () => loadData('posts')
+  },
+  {
+    path: '/partners',
+    exact: true,
+    component: Test,
+    loadData: () => loadData('posts')
+  },
+  {
+    path: '/privacy',
+    exact: true,
+    component: Test,
+    loadData: () => loadData('posts')
+  },
+  {
+    path: '/products',
+    exact: true,
+    component: Test,
+    loadData: () => loadData('posts')
+  },
+  {
+    path: '/whats-new',
+    exact: true,
+    component: Test,
+    loadData: () => loadData('posts')
+  },
+  {
+    path: '/success-stories',
+    exact: true,
+    component: Test,
+    loadData: () => loadData('posts')
+  },
+  {
+    path: '/why-robotmaster',
+    exact: true,
+    component: Test,
+    loadData: () => loadData('posts')
+  },
+  {
     component: NotFound
   }
 ];
