f3ab86ec2ce3097472ec8fdb40dd02f8e61ae359
Szabó Bence <szabo.bence.tat@gmail.com>
2020-04-13 16:08:36 +0200

    app-s3

--- src/server/index.js
+++ src/server/index.js
@@ -3,11 +3,13 @@ import express from 'express';
 
 import htmlMiddleware from './middlewares/html';
 import renderMiddleware from './middlewares/render';
+import s3 from './services/s3/s3';
 
 const publicPath = path.join(__dirname, '/public');
 const app = express();
 
 app.use(express.static(publicPath));
+app.use("/s3", s3());
 app.use(htmlMiddleware());
 app.use(renderMiddleware());
 
---
+++ src/server/services/s3/s3.js
@@ -0,0 +1,25 @@
+import knox from 'knox';
+const client = knox.createClient({
+      key: process.env.AWS_ACCESS_KEY_ID,
+      secret: process.env.AWS_SECRET_KEY,
+      bucket: "robotmaster-website"
+    });
+
+export default function() {
+  return function(req, res) {
+    client.getFile(req.path, function(err, resp) {
+      if (err) {
+        console.log(err);
+        res.send(500);
+      } else {
+        if (resp.statusCode !== 200) {
+          res.send(resp.statusCode);
+        } else {
+          res.setHeader("Content-Length", resp.headers["content-length"]);
+          res.setHeader("Content-Type", resp.headers["content-type"]);
+          resp.pipe(res);
+        }
+      }
+    });
+  };
+};
