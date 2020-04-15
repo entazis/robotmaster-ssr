a53f67d6eab29a9628a5aa187d9335c9e28c13ca
Szabó Bence <szabo.bence.tat@gmail.com>
2020-04-15 08:05:46 +0200

    grouping api call handling

--- src/server/index.js
+++ src/server/index.js
@@ -4,15 +4,16 @@ import express from 'express';
 import htmlMiddleware from './middlewares/html';
 import renderMiddleware from './middlewares/render';
 import fetchMiddleware from "./middlewares/fetch";
-import s3 from './services/s3/s3';
+import s3 from './api/s3/s3';
 
 const publicPath = path.join(__dirname, '/public');
 const app = express();
 
 app.use(express.static(publicPath));
-app.use("/s3", s3());
-app.use(htmlMiddleware());
+app.use('/s3', s3());
+
 app.use(fetchMiddleware());
+app.use(htmlMiddleware());
 app.use(renderMiddleware());
 
 export default app;
